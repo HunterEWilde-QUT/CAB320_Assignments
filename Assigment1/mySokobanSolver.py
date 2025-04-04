4d5cdcb11f8b935db2f97254e2bfe22cdff947fe
Hunter Wilde <hunter.wilde@connect.qut.edu.au>
2025-04-04 13:13:42 +1000

    Began implementing `SokobanPuzzle`
    Wrote boilerplate for the `__init__`, `actions`, & `result` methods of the `SokobanPuzzle` class; implemented `__init__`, but needs testing; cleaned up some documentation formatting for consistency.

--- Assigment1/mySokobanSolver.py
+++ Assigment1/mySokobanSolver.py
@@ -1,30 +1,29 @@
-
-'''
+"""
 
     Sokoban assignment
 
 
-The functions and classes defined in this module will be called by a marker script. 
+The functions and classes defined in this module will be called by a marker script.
 You should complete the functions and classes according to their specified interfaces.
 
 No partial marks will be awarded for functions that do not meet the specifications
 of the interfaces.
 
 You are NOT allowed to change the defined interfaces.
-In other words, you must fully adhere to the specifications of the 
+In other words, you must fully adhere to the specifications of the
 functions, their arguments and returned values.
-Changing the interfacce of a function will likely result in a fail 
-for the test of your code. This is not negotiable! 
+Changing the interfacce of a function will likely result in a fail
+for the test of your code. This is not negotiable!
 
-You have to make sure that your code works with the files provided 
-(search.py and sokoban.py) as your code will be tested 
-with the original copies of these files. 
+You have to make sure that your code works with the files provided
+(search.py and sokoban.py) as your code will be tested
+with the original copies of these files.
 
 Last modified by 2021-08-17  by f.maire@qut.edu.au
 - clarifiy some comments, rename some functions
   (and hopefully didn't introduce any bug!)
 
-'''
+"""
 
 # You have to make sure that your code works with 
 # the files provided (search.py and sokoban.py) as your code will be tested 
@@ -37,77 +36,83 @@ import sokoban
 
 
 def my_team():
-    '''
+    """
     Return the list of the team members of this assignment submission as a list
     of triplet of the form (student_number, first_name, last_name)
-    
-    '''
+    """
     return [ (11032553, 'Hunter', 'Wilde'), (12026395, 'Oliver', 'Kele') ]
-    raise NotImplementedError()
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
 def taboo_cells(warehouse):
-    '''  
+    """
     Identify the taboo cells of a warehouse. A "taboo cell" is by definition
-    a cell inside a warehouse such that whenever a box get pushed on such 
-    a cell then the puzzle becomes unsolvable. 
-    
+    a cell inside a warehouse such that whenever a box get pushed on such
+    a cell then the puzzle becomes unsolvable.
+
     Cells outside the warehouse are not taboo. It is a fail to tag one as taboo.
-    
-    When determining the taboo cells, you must ignore all the existing boxes, 
-    only consider the walls and the target  cells.  
+
+    When determining the taboo cells, you must ignore all the existing boxes,
+    only consider the walls and the target  cells.
     Use only the following rules to determine the taboo cells;
      Rule 1: if a cell is a corner and not a target, then it is a taboo cell.
-     Rule 2: all the cells between two corners along a wall are taboo if none of 
+     Rule 2: all the cells between two corners along a wall are taboo if none of
              these cells is a target.
-    
-    @param warehouse: 
+
+    @param warehouse:
         a Warehouse object with a worker inside the warehouse
 
     @return
-       A string representing the warehouse with only the wall cells marked with 
-       a '#' and the taboo cells marked with a 'X'.  
+       A string representing the warehouse with only the wall cells marked with
+       a '#' and the taboo cells marked with a 'X'.
        The returned string should NOT have marks for the worker, the targets,
-       and the boxes.  
-    '''
-    ##         "INSERT YOUR CODE HERE"    
+       and the boxes.
+    """
+    ##         "INSERT YOUR CODE HERE"
     raise NotImplementedError()
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
 class SokobanPuzzle(search.Problem):
-    '''
+    """
     An instance of the class 'SokobanPuzzle' represents a Sokoban puzzle.
     An instance contains information about the walls, the targets, the boxes
     and the worker.
 
-    Your implementation should be fully compatible with the search functions of 
-    the provided module 'search.py'. 
-    
-    '''
-    
-    #
-    #         "INSERT YOUR CODE HERE"
-    #
-    #     Revisit the sliding puzzle and the pancake puzzle for inspiration!
-    #
-    #     Note that you will need to add several functions to 
-    #     complete this class. For example, a 'result' method is needed
-    #     to satisfy the interface of 'search.Problem'.
-    #
-    #     You are allowed (and encouraged) to use auxiliary functions and classes
-
-    
+    Your implementation should be fully compatible with the search functions of
+    the provided module 'search.py'.
+    """
     def __init__(self, warehouse):
-        raise NotImplementedError()
+        """
+        Writes the lines of the warehouse file into the `rows` array;
+        searches each row for the initial state (i.e. the player's position '@')
+        & the goal state (i.e. the target square's position '.'),
+        the indices of which are then stored in the `initial` and `goal` variables.
+        :param warehouse: text file mapping the warehouse layout.
+        """
+        file = open(warehouse, 'r')
+        rows = file.readlines()
+        for row in rows:
+            if '@' in row:
+                self.initial = (rows.index(row), row.index('@'))
+            if '.' in row:
+                self.goal = (rows.index(row), row.index('.'))
 
     def actions(self, state):
         """
         Return the list of actions that can be executed in the given state.
-        
+        :param state: a given state.
+        """
+        raise NotImplementedError
+
+    def result(self, state, action):
+        """
+        Applies the given action to the given state and returns the resulting state.
+        :param state: a given state.
+        :param action: action to be applied.
+        :return: state resulting from applying the action to the given state.
         """
         raise NotImplementedError
 
